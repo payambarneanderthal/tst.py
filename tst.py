fce587b579d20d8cb3bae79ad05f233b57d3fe8b
payambarneanderthal <99961787+payambarneanderthal@users.noreply.github.com>
2022-02-23 07:05:02 +0330

    Create tst.py

---
+++ tst.py
@@ -0,0 +1,19 @@
+# SECURITY 
+
+import requests
+
+import time
+
+target = input("enter number >>> ")
+
+url = "https://web.rubika.ir"
+
+while True:
+
+time.sleep(3)
+
+pyload ={"cellphone": target}
+
+u = requests.post(url, data=pyload)
+
+print(be fuck raft)
